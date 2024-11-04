55c3f66011db4109e36096e4874d31efe027ae96
nelellen <nelsonellen80@yahoo.com>
2024-11-04 18:32:26 -0500

    commit all images

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = '5ffc3fe05c8cbec15b1f8fdd119944e3'
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "ee5ea7ad2ea64f73904988fc5880494d"
